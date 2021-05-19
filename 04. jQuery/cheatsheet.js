54abdb6d93d4af0478cd77aecc7dc5d9f3f85b83
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-05-19 14:20:21 +0530

    Added jquery files

--- 04. jQuery/cheatsheet.js
+++ 04. jQuery/cheatsheet.js
@@ -1,136 +1,117 @@
-
 $(document).ready(function () {
-    console.log('I am in a new file now');
+  console.log("I am in a new file now");
 
-    //Your jquery code here
-    console.log("We are using jQuery");
- 
-    // jQuery Syntax looks like this
-    // $('selector').action()
+  //Your jquery code here
+  console.log("We are using jQuery");
 
-    //clicks on all the p elements.
-    // $('p').click(); //click on p
-    // $('p').click(function () {
-    //     console.log('you clicked on p', this);
-    //     //  $('#id').hide();
-    //     //  $('.class').hide();
-    // }); //do this when we click on p
+  // jQuery Syntax looks like this
+  // $('selector').action()
 
+  //clicks on all the p elements.
+  // $('p').click(); //click on p
+  // $('p').click(function () {
+  //     console.log('you clicked on p', this);
+  //     //  $('#id').hide();
+  //     //  $('.class').hide();
+  // }); //do this when we click on p
 
-    // $('p').dblclick(function () {
-    //     console.log('you double clicked on p', this);
-    //     //  $('#id').hide();
-    //     //  $('.class').hide();
-    // });
+  // $('p').dblclick(function () {
+  //     console.log('you double clicked on p', this);
+  //     //  $('#id').hide();
+  //     //  $('.class').hide();
+  // });
 
-    
-    // $('p').hover(function () {
-    //     console.log('you hoverd on: ', this);
-    //     //  $('#id').hide();
-    //     //  $('.class').hide();
-    // },
-    // function (){
+  // $('p').hover(function () {
+  //     console.log('you hoverd on: ', this);
+  //     //  $('#id').hide();
+  //     //  $('.class').hide();
+  // },
+  // function (){
 
-    //     console.log('Thanks for coming')
-    // });
+  //     console.log('Thanks for coming')
+  // });
 
+  // there are three main types of selectors in jQuery
+  // 1. element selector
+  // 2. id selector
+  // 3. class selector
 
-    // there are three main types of selectors in jQuery 
-    // 1. element selector
-    // 2. id selector
-    // 3. class selector
+  // 1. Element selector -  This is an example of element selector which clicks on all p
+  // $('p').click();
 
-    // 1. Element selector -  This is an example of element selector which clicks on all p
-    // $('p').click();
+  // 2. Id selector - this is an example of id selector
+  // $('#second').click();
 
-    // 2. Id selector - this is an example of id selector
-    // $('#second').click();
+  // 3. Class selector - this is an example of id selector
+  // $('.odd').click();
 
-    // 3. Class selector - this is an example of id selector
-    // $('.odd').click();
+  // other selectors
+  // $('*').click() // clicks on all the elements
+  // $('p.odd').click() // clicks on all the elements
+  // $('p:first').click() // clicks on all the elements
 
-    // other selectors
-    // $('*').click() // clicks on all the elements
-    // $('p.odd').click() // clicks on all the elements
-    // $('p:first').click() // clicks on all the elements
+  // Events in jQuery
+  // Mouse events = click, dblclick, mouseenter, mouseleave
+  // KeyboardEvent = keypress, keydown, MediaKeyStatusMap
+  // form events = submit, change, focus, blur
+  // document/window events = load, resize, scroll, unload
 
-    // Events in jQuery
-    // Mouse events = click, dblclick, mouseenter, mouseleave
-    // KeyboardEvent = keypress, keydown, MediaKeyStatusMap
-    // form events = submit, change, focus, blur
-    // document/window events = load, resize, scroll, unload
+  // demoing the on method
+  $("p").on({
+    click: function () {
+      console.log("Thanks for clicking", this);
+    },
+    mouseleave: function () {
+      console.log("mouseleave");
+    },
+  });
 
-    // demoing the on method
-    $('p').on(
-        {
-            click: function () {
-                console.log('Thanks for clicking', this);
-            },
-            mouseleave: function () {
-                console.log("mouseleave");
+  // $('#wiki').hide(1000, function () {
+  //     console.log("hidden");
+  // })
+  // $('#wiki').show(1000, function () {
+  //     console.log("show");
+  // })
+  $("#but").click(function () {
+    $("#wiki").fadeOut(5000);
+  });
 
-            }
+  // fadeIn()
+  // fadeOut()
+  // fadeToggle()
+  // fadeTo()
 
+  // Slide methods - speed and callback parameters are optional
+  // $('#wiki').slideUp(1000, function(){
+  //     console.log('done');
+  // })
+  // $('#wiki').slideDown(1000)
+  // $('#wiki').slideToggle(1000)
 
+  // Animate function in jQuery
+  // $('#wiki').animate({
+  //     opacity:0.3,
+  //     height: '150px',
+  //     width:'350px'
 
-        })
+  // }, "fast")
+  // $('#wiki').animate({ opacity: 0.3 }, 4000);
+  // $('#wiki').animate({ opacity: 0.9 }, 1000);
+  // $('#wiki').animate({ width: '350px' }, 12000);
 
-    // $('#wiki').hide(1000, function () {
-    //     console.log("hidden");
-    // })   
-    // $('#wiki').show(1000, function () {
-    //     console.log("show");
-    // })  
-    $('#but').click(function () {
-        $('#wiki').fadeOut(5000);
-    })
+  // $('#ta').val('setting it to harry');
+  // $('#ta').html('setting it to harry');
+  // $('#ta').html('setting it to harry3');
+  // $('#inp').html('setting it to harry3');
+  // $('#inp').val('setting it to harry3');
+  // $('#inp').empty()
+  // $('#wiki').empty()
+  // $('#wiki').text('you are good')
+  // $('#wiki').remove()
 
-    //     fadeIn()
-    // fadeOut()
-    // fadeToggle()
-    // fadeTo()
-
-    // Slide methods - speed and callback parameters are optional
-    // $('#wiki').slideUp(1000, function(){
-    //     console.log('done');
-    // })
-    // $('#wiki').slideDown(1000)
-    // $('#wiki').slideToggle(1000)
-
-    // Animate function in jQuery
-    // $('#wiki').animate({
-    //     opacity:0.3,
-    //     height: '150px',
-    //     width:'350px'
-
-    // }, "fast")
-    // $('#wiki').animate({ opacity: 0.3 }, 4000);
-    // $('#wiki').animate({ opacity: 0.9 }, 1000);
-    // $('#wiki').animate({ width: '350px' }, 12000);
-
-    // $('#ta').val('setting it to harry');
-    // $('#ta').html('setting it to harry');
-    // $('#ta').html('setting it to harry3');
-    // $('#inp').html('setting it to harry3');
-    // $('#inp').val('setting it to harry3');
-    // $('#inp').empty()
-    // $('#wiki').empty()
-    // $('#wiki').text('you are good')
-    // $('#wiki').remove()
-
-    //     $('#wiki').addClass('myclass')
-    // $('#wiki').addClass('myclass2')
-    // $('#wiki').removeClass('myclass2')
-    // $('#wiki').css('background-color', 'red')
-    // $('#wiki').css('background-color')
-
-    // AJAX Using jQuery
-    // $.get('https://code.jquery.com/jquery-3.3.1.js', function (data, status) { alert(data); })
-
-    // $.get('https://code.jquery.com/jquery-3.3.1.js', function (data, status) { alert(status); })
-
-    // $.post('https://code.jquery.com/jquery-3.3.1.js',
-    //    
-    //     function (data, status) { alert(status) });
-
+  //     $('#wiki').addClass('myclass')
+  // $('#wiki').addClass('myclass2')
+  // $('#wiki').removeClass('myclass2')
+  // $('#wiki').css('background-color', 'red')
+  // $('#wiki').css('background-color')
 });
--- 04. jQuery/ufoGame/script.js
+++ 04. jQuery/ufoGame/script.js
@@ -3,82 +3,79 @@ document.getElementByID("spaceship");
 in jQuery
 $('#spaceship)
 */
-$(document).ready(function(){
+$(document).ready(function () {
+  var spaceship = $("#spaceship");
 
-    var spaceship = $('#spaceship');
+  //Changing css via jQuery
+  spaceship.css({
+    //css function
+    position: "absolute", // Which is static by default
+    left: 500,
+    top: 200,
+    width: 160,
+    height: 120,
+  });
 
-    //Changing css via jQuery
-    spaceship.css({     //css function
-        'position' : 'absolute',  // Which is static by default
-        'left' : 500,
-        'top': 200,
-        'width': 160,
-        'height': 120
-    })
-    
-    //Now i want to change the spaceship position using my keys
-    var ismovingRight = false;
-    var ismovingDown = false;
-    var ismovingUp = false;
-    var ismovingLeft = false;
-    //keydown event
-    $(document).on("keydown", function(e){
-        /*Cheaking keyCode */
-        console.log(e.keyCode)
-       
-       var kc = e.keyCode;
-       if(kc == 39){
-          ismovingRight = true;
-       }
-       if(kc == 40){
-           ismovingDown = true;
-       }
-       if(kc == 37){
-        ismovingLeft = true;
-        }
-        if(kc == 38){
-            ismovingUp = true;
-        }
-    });
-    
-       
-    //keyup event
-    $(document).on("keyup", function(e){
-       var kc = e.keyCode;
-        if(kc == 39){
-          ismovingRight = false;
-        }
-        if(kc == 40){
-        ismovingDown = false;
-        }
-        if(kc == 37){
-         ismovingLeft = false;
-        }
-        if(kc == 38){
-          ismovingUp = false;
-        }
-    });
+  //Now i want to change the spaceship position using my keys
+  var ismovingRight = false;
+  var ismovingDown = false;
+  var ismovingUp = false;
+  var ismovingLeft = false;
+  //keydown event
+  $(document).on("keydown", function (e) {
+    /*Cheaking keyCode */
+    console.log(e.keyCode);
 
+    var kc = e.keyCode;
+    if (kc == 39) {
+      ismovingRight = true;
+    }
+    if (kc == 40) {
+      ismovingDown = true;
+    }
+    if (kc == 37) {
+      ismovingLeft = true;
+    }
+    if (kc == 38) {
+      ismovingUp = true;
+    }
+  });
 
-    //writing function for smooth flow of spaceship
-    var speed = 6
-    function move(){
-        if(ismovingRight){
-            spaceship.css('left', (spaceship.position().left + speed))
-        }
-        if(ismovingDown){
-            spaceship.css('top', (spaceship.position().top + speed))
-        }
-        if(ismovingUp){
-            spaceship.css('top', (spaceship.position().top  -speed))
-        }
-        if(ismovingLeft){
-            spaceship.css('left', (spaceship.position().left - speed))
-        }
-    }      
-    
-    setInterval(move , 10)
+  //keyup event
+  $(document).on("keyup", function (e) {
+    var kc = e.keyCode;
+    if (kc == 39) {
+      ismovingRight = false;
+    }
+    if (kc == 40) {
+      ismovingDown = false;
+    }
+    if (kc == 37) {
+      ismovingLeft = false;
+    }
+    if (kc == 38) {
+      ismovingUp = false;
+    }
+  });
 
+  //writing function for smooth flow of spaceship
+  var speed = 6;
+  function move() {
+    if (ismovingRight) {
+      spaceship.css("left", spaceship.position().left + speed);
+    }
+    if (ismovingDown) {
+      spaceship.css("top", spaceship.position().top + speed);
+    }
+    if (ismovingUp) {
+      spaceship.css("top", spaceship.position().top - speed);
+    }
+    if (ismovingLeft) {
+      spaceship.css("left", spaceship.position().left - speed);
+    }
+  }
+
+  setInterval(move, 10);
 });
 
 // Can also be used to make snake games
