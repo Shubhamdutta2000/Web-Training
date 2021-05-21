c170b8a40c8478e75fbd2e45980de0f81220f09a
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-05-21 22:50:09 +0530

    added ejs module and update node express module

--- 11. Node/Functional Expression.js
+++ 11. Node/Functional Expression.js
@@ -15,5 +15,4 @@ function callFunction(fun){
     fun();
 }
 
-
 callFunction(sayBye);
--- 11. Node/expressRoute/Routes.js
+++ 11. Node/expressRoute/Routes.js
@@ -1,23 +1,12 @@
-/*
- npm == node package manager
- It is a 3rd party package system means
- open source of libery for node js which helps us to perform certain types of task
- We can find aanything in it
-
- site: https://www.npmjs.com
-
-
-*/
-
 //Creating Server Using express
 
 const express = require("express");
 
-/*New method based on ES6
-include express from 'express';
-
-ES6 is a set of rules for writing modern js (optional needs to learn valina js first)
+/*
+  New method based on ES6
+  include express from 'express';
 */
+
 const app = express();
 
 app.get("/", function (req, res) {
