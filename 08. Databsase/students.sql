9a65f0a6481c960e39297c69261c4959067ec849
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-09-15 20:18:59 +0530

    [NEW] added sql demo code

---
+++ 08. Databsase/students.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE test2 ;
+
+USE test2;
+
+///////////////////////////////////// Create Table ////////////////////////////////////////////////
+
+CREATE TABLE `students` (
+    `student_name` VARCHAR(255) NOT NULL ,
+    `student_roll` INT(200) NOT NULL ,
+    `marks` INT(100) NOT NULL ,
+    `percenntage` INT(100) NOT NULL ,
+    PRIMARY KEY (`student_roll`)
+    );
+
+
+
+/////////////////////////////////////   INSERT new Row in the table ////////////////////////////////
+
+INSERT INTO `students` (
+    `student_name`, `student_roll`, `marks`, `percenntage`
+    ) VALUES ('Avik', '12', '99', '99');
+
+
+
+///////////////////////////////////// DELETE particular Row /////////////////////////////////////
+
+DELETE FROM `students` WHERE `students`.`student_roll` = 11";
+
+
+
+/////////////////////////////////////  UPDATE COL in particular Row /////////////////////////////////////
+
+UPDATE `students` SET `student_name` = 'Avik2' WHERE `students`.`student_roll` = 12; 
+
+
+
+///////////////////////////////////// READ particular row from the table  /////////////////////////////////////
+
+SELECT * FROM `students`;
+
+SELECT * FROM `students` WHERE col_name = value;
